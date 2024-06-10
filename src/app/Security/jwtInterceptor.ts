3e07cc9b843912031a1ceb6765e0b9e95cb416d2
Kevin Cuba Castillo <cck2023657@est.univalle.edu>
2024-06-09 23:49:51 -0400

    cambios finales ahora si de a devis, creo

--- src/app/Security/jwtInterceptor.ts
+++ src/app/Security/jwtInterceptor.ts
@@ -1,11 +1,40 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { ApiCancionesService } from "../services/api-canciones.service";
-import { Observable } from "rxjs";
+import { Observable, finalize } from "rxjs";
 import { ApiAuthService } from "../services/api-auth.service";
+import { NgxUiLoaderService } from "ngx-ui-loader";
 
 @Injectable()
 export class jwtInterceptor implements HttpInterceptor{
+    constructor(private _apiService : ApiAuthService, private _loader : NgxUiLoaderService){
+
+    }
+    private _activeRequest = 0;
+    intercept(request: HttpRequest<any>, next: HttpHandler):Observable<HttpEvent<any>>{
+        if(this._activeRequest === 0){
+            this._loader.start();
+        }
+        this._activeRequest++;
+        const usuario = this._apiService.userData;
+        if(usuario){
+            request = request.clone({
+                setHeaders: {
+                    Authorization: `Bearer ${usuario.token}`
+                }
+            });
+        }
+        return next.handle(request).pipe(finalize(() => this.stopLoader()));
+    }
+    private stopLoader(){
+        this._activeRequest--;
+        if(this._activeRequest === 0){
+            this._loader.stop();
+        }
+    }
+}
+
+/*export class jwtInterceptor implements HttpInterceptor{
     constructor(private _apiService : ApiAuthService){
 
     }
@@ -20,4 +49,4 @@ export class jwtInterceptor implements HttpInterceptor{
         }
         return next.handle(request);
     }
-}
+}*/
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,13 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { userLogin } from './Models/userLogin';
 import { ApiCancionesService } from './services/api-canciones.service';
+import { NgxUiLoaderComponent, NgxUiLoaderModule } from 'ngx-ui-loader';
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, HeaderComponent, DashboardComponent, FooterComponent, MiniReproductorComponent, HttpClientModule, ReactiveFormsModule]
+    imports: [CommonModule, RouterOutlet, HeaderComponent, DashboardComponent, FooterComponent, MiniReproductorComponent, HttpClientModule, ReactiveFormsModule,NgxUiLoaderModule]
 })
 export class AppComponent {
   title = 'Spoty_app';
